1d764f1829ae03ae9d87ee7189ea7bd6c62af894
isaacpitwa <isaacpitwa256@gmail.com>
2022-03-23 02:41:24 +0300

    Set up HTML template

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function component() {
+    const element = document.createElement('div');
+  
+    element.innerHTML = "Hello Webpack";
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+  
